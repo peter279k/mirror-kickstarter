7fbc8310a83f9c1927701897e209fbda1dd39b4f
peter279k <peter279k@gmail.com>
2020-12-16 00:38:00 +0800

    Update mirror updater script

--- mirror-updater.sh
+++ mirror-updater.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 
 green_color='\e[0;32m'
+yellow_color='\e[0;33m'
 rest_color='\e[0m'
+
+if [ ${USER} != "root" ]; then
+    sudo_prefix='sudo '
+fi;
 
 echo -e "${green_color}Welcome to the kickstarter for Composer mirror updater!${rest_color}"
 
 echo -e "${green_color}Update composer/mirror repository form GitHub...${rest_color}"
 
-${sudo_prefix}su -p -l www-data -s /bin/bash -c "cd /var/www/html/mirror/ && git pull origin master"
+${sudo_prefix}su -p -l www-data -s /bin/bash -c "cd /var/www/html/mirror/ && git pull origin master --no-edit"
 ${sudo_prefix}su -p -l www-data -s /bin/bash -c "cd /var/www/html/mirror/ && composer install"
+
+echo "${yellow_color}Restarting worker...${rest_color}"
+${sudo_prefix}systemctl restart supervisor
 
 echo "${green_color}Update Composer Repository Mirror has been done.${rest_color}"
